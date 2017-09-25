5adc54a6d645cb34951ce4ef9413f64fc4ed8f86
Magnus <magnus.svedjebratt@gmail.com>
2017-09-25 14:53:38 +0200

    Added typings.

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export function addBundle(bundle: {}): void;
+
+export default function getMessage(key: string, ...arg: any[]): any;
+
+export function getHtmlMessage(key: string, ...arg: any[]): any;
